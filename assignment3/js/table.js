bc35a750849b46df7bb89764ca03f970d0bb05e3
Jelani Thompson <jthompson34@myseneca.ca>
2017-03-05 23:08:45 -0500

    Fin.

--- assignment3/js/table.js
+++ assignment3/js/table.js
@@ -1,5 +1,4 @@
 // Data for the "HTML Tables" Page
-
 var users = [
     {first_name: "Kaitlin", last_name: "Burns", age: 23, email: "kburns99753@usermail.com"},
     {first_name: "Joshua", last_name: "Feir", age: 31, email: "josh319726@usermail.com"},
@@ -7,3 +6,18 @@ var users = [
     {first_name: "Timothy", last_name: "McAlpine", age: 37, email: "Timbo72469@usermail.com"},
     {first_name: "Sarah", last_name: "Connor", age: 19, email: "SarahC6320@usermail.com"}
 ];
+
+window.onload = function() {
+    let tables = document.querySelector("#dat-table");
+    let rows = "";
+    
+    for (var i = 0; i < users.length; i++) {
+		rows += "<tr>" + "<td>" + 
+        users[i].first_name + "</td>" + "<td>" +
+        users[i].last_name + "</td>" + "<td>" + 
+        users[i].age + "</td>" + "<td>" + '<a href="mailto:someone@example.com?Subject=Hello%20again">' + 
+        users[i].email + " </a>" + " </td>" + "</tr>";
+    }
+
+    tables.innerHTML += rows;  
+};
